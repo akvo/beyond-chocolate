0f1171379f4c2ca1c38ff5333cb86d15cb140ab9
dedenbangkit <mail@dedenbangkit.com>
2021-07-21 18:09:02 +0700

    [#1] build and deploy script

---
+++ ci/build-semaphore.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+#cp .env.prod .env
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/gisco-demo" \
+       --workdir /home/tcakvo/public_html/gisco-demo \
+       --entrypoint /bin/sh \
+       composer:1.10.17 -c 'composer install'
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/gisco-demo" \
+       --workdir /home/tcakvo/public_html/gisco-demo \
+       --entrypoint /bin/sh \
+       composer:1.10.17 -c 'composer dump-autoload'
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/gisco-demo" \
+       --workdir "/home/tcakvo/public_html/gisco-demo" \
+       --entrypoint /bin/sh \
+       node:8-alpine -c 'npm i && npm run prod'
